f6b704a0007d18b7460a48e829029032471623bd
Kanstantsin Mikhaltsevich <k_mikhaltsevich@wargaming.net>
2024-02-11 15:15:47 +0200

    Add vite-plugin-chunk-split

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,8 +1,14 @@
-import styled from '@emotion/styled';
+import { ReactNode } from 'react';
 
-export const Container = styled.div`
-  max-width: ${(props: { large?: boolean }) => props.large ? '1600px' : '1280px'};
-  width: 100%;
-  margin: 0 auto;
-  padding: 0 16px;
-`
+export const Container = ({ children, large }: { children: ReactNode, large?: boolean }) => {
+    const containerStyles = {
+        maxWidth: large ? '1600px' : '1280px',
+        width: '100%',
+        margin: '0 auto',
+        padding: '0 16px',
+    };
+
+    return (
+        <div style={containerStyles}>{children}</div>
+    );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true
-  },
-  plugins: [react()],
-})
+    server: {
+        port: 3000,
+        open: true,
+    },
+    plugins: [
+        react(),
+        chunkSplitPlugin({
+            strategy: 'unbundle',
+            customChunk: (args)=>{
+                // files into pages directory is export in single files
+                let { file }: any = args;
+
+                if(file.startsWith('src/pages/')){
+                    file = file.substring(4);
+                    file = file.replace(/\.[^.$]+$/, '');
+                    return file;
+                }
+                return null;
+            },
+        })
+    ],
+    // build: {
+    //     rollupOptions: {
+    //         output: {
+    //             manualChunks(id) {
+    //                 if (id.includes('node_modules')) {
+    //                     return id.toString().split('node_modules/')[1].split('/')[0].toString();
+    //                 }
+    //             },
+    //         },
+    //     },
+    // },
+});
